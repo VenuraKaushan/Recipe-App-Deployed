76d39c5b94b97ffbf685ce8b055d663d57ceff04
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2024-10-23 20:56:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://recipe-app-deployed-backend.vercel.app",
-        secure: false,
+        secure: true,
       },
     },
   },
