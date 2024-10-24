fc4c8f101d2992bbc5daf7f44d9c51bb59c313dc
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2024-10-24 22:42:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 6001;
 
 // CORS configuration
-app.use(cors());
+app.use(cors({
+  origin: 'https://recipe-app-deployed-frontend.vercel.app', // Allowed origin (frontend)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-Token'] // Allowed headers
+}));
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
